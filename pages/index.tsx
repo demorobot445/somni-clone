cef05d899a3ed3cf31d2639384458eb28d14c16a
Robot <demo11website@gmail.com>
2026-03-03 14:55:58 +0500

    update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Somni Clone</title>
+        <title>Hala Coffee</title>
       </Head>
       <HeroV2 />
       {/* <Hero /> */}
